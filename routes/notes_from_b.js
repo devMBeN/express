c1431c847150086526d6882a981910cf9d613417
devMBeN <dev@benice.jp>
2024-07-21 19:06:41 +0900

    notes_from_b.jsを更新

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const client = new MongoClient("mongodb+srv://devMBeN:UdwX8lU%7BAdr9-o%23D%40mdb-@cluster0.9gweval.mongodb.net/?retryWrites=true&w=majority");
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
 router.use(cors());
@@ -13,6 +15,7 @@ router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
+
 
 // 全てのドキュメントを取得
 const note = await notes.find({}).toArray();
