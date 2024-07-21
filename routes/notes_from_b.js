839c40dd87fea79612cfe5bbe397b7694847cd55
devMBeN <dev@benice.jp>
2024-07-21 18:39:47 +0900

    env2

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
-require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+const client = new MongoClient("mongodb+srv://devMBeN:UdwX8lU%7BAdr9-o%23D%40mdb-@cluster0.9gweval.mongodb.net/?retryWrites=true&w=majority");
 
 // corsミドルウェアを使用
 router.use(cors());
